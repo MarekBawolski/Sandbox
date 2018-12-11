67393efe603378e33f00480e227831ef0926fb86
Marek <Marek@DESKTOP-E8MR8CJ>
2018-12-11 14:56:59 +0100

    first commit

---
+++ Sandbox/PoPllikach.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    class PoPllikach
+    {
+        private string[] dir;
+        private List<String> pliki;
+
+
+        public PoPllikach()
+        {
+            this.dir =  Directory.GetFiles(@"C:\Users\Marek\Downloads\excel\input");
+            this.pliki = new List<string>();
+
+        }
+
+        public List<string> printFiles() {
+            foreach (string path in dir)
+            {
+                this.pliki.Add(path);
+            }
+            return pliki;
+        }
+    }
+}
---
+++ Sandbox/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read_From_Excel.getExcelFile();
+            PoPllikach pp = new PoPllikach();
+            List<string> pliki = pp.printFiles();
+
+            foreach (string plik in pliki) {
+                Console.WriteLine(plik);
+            }
+
+
+            foreach (string plik in pliki)
+            {
+                Read_From_Excel.getExcelFile(plik);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
