3097148e87a6a7a837d028ddedc50e4fd4bb1636
Marek <Marek@DESKTOP-E8MR8CJ>
2018-12-13 15:24:58 +0100

    webfhwuebf

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using CsvHelper;
 
 namespace Sandbox
 {
@@ -10,21 +13,44 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            //Read_From_Excel.getExcelFile();
             PoPllikach pp = new PoPllikach();
             List<string> pliki = pp.printFiles();
-
-            foreach (string plik in pliki) {
-                Console.WriteLine(plik);
-            }
-
+            StreamWriter writer = new StreamWriter(@"C:\Users\Marek\Downloads\excel\out\log.txt", true);
 
             foreach (string plik in pliki)
             {
-                Read_From_Excel.getExcelFile(plik);
-            }
+                MatchCollection matches = Regex.Matches(plik, "POL.........");
+                List<string> chemia = Read_From_Excel.getExcelFile(plik, writer);
 
-            Console.ReadLine();
+
+
+                foreach (Match ma in matches)
+                {
+
+                    if (chemia.Count<2) {
+                        continue;
+                    }
+                    string content = ma +";";
+                    string currentPlik = plik;
+                    foreach (string chemikal in chemia)
+                    {
+                        if (chemikal == null)
+                        {
+                            content += "null";
+                        }
+                        content += chemikal+";";
+
+
+                    }
+                    using (StreamWriter w = File.AppendText(@"C:\Users\Marek\Downloads\excel\out\output.csv"))
+                    {
+                        w.WriteLine(content);
+                    }
+                }
+                
+
+
+            }
 
         }
     }
--- Sandbox/Read_From_Excel.cs
+++ Sandbox/Read_From_Excel.cs
@@ -11,8 +11,11 @@ namespace Sandbox
 {
     public class Read_From_Excel
     {
-        public static void getExcelFile(string path)
+        public static List<string> getExcelFile(string path, StreamWriter writer)
         {
+
+            List<string> chemia = new List<string>();
+
 
             //Create COM Objects. Create a COM object for everything that is referencedddd
             Excel.Application xlApp = new Excel.Application();
@@ -23,21 +26,77 @@ namespace Sandbox
             int rowCount = xlRange.Rows.Count;
             int colCount = xlRange.Columns.Count;
 
-            //iterate over the rows and columns and print to the console as it appears in the file
-            //excel is not zero based!!
-            for (int i = 1; i <= rowCount; i++)
-            {
-                for (int j = 1; j <= colCount; j++)
-                {
-                    //new line
-                    if (j == 1)
-                        Console.Write("\r\n");
 
-                    //write the value to the console
-                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
-                        Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
-                }
+            Excel.Range currentFind = null;
+            Excel.Range Chempol = xlWorksheet.UsedRange;
+            currentFind = Chempol.Find("Lp.", Type.Missing,
+                Excel.XlFindLookIn.xlValues, Excel.XlLookAt.xlPart,
+                Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlNext, false,
+                Type.Missing, Type.Missing);
+
+            if (currentFind == null) {
+                using (StreamWriter sw = writer) {
+                    writer.WriteLine(path + "  nie znaleziono 'Lp.'");
+                        }
+                Marshal.ReleaseComObject(xlRange);
+                Marshal.ReleaseComObject(xlWorksheet);
+                xlWorkbook.Close();
+                Marshal.ReleaseComObject(xlWorkbook);
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+                return new List<string>();
+
             }
+
+            int m = currentFind.Row;
+            int LpStart = m;
+            int n = currentFind.Column;
+
+            string rowPosition = (string)(xlWorksheet.Cells[m, n] as Excel.Range).Value2;
+
+            while (rowPosition != "1")
+            {
+                m += 1;
+                rowPosition = ToStr((xlWorksheet.Cells[m, n] as Excel.Range).Value2);
+
+            }
+
+            string columnPosition = ToStr((xlWorksheet.Cells[m, n] as Excel.Range).Value2);
+
+            while (columnPosition != "Rodzaj farby/rozpuszczalnika/ aktywatora/lakieru")
+            {
+                n += 1;
+                columnPosition = ToStr((xlWorksheet.Cells[LpStart, n] as Excel.Range).Value2);
+
+                if (n > 100) {
+
+                    using (StreamWriter sw = writer)
+                    {
+                        writer.WriteLine(path + "  nie znaleziono 'Rodzaj farby/rozpuszczalnika/ aktywatora/lakieru'");
+                    }
+                    Marshal.ReleaseComObject(xlRange);
+                    Marshal.ReleaseComObject(xlWorksheet);
+                    xlWorkbook.Close();
+                    Marshal.ReleaseComObject(xlWorkbook);
+                    xlApp.Quit();
+                    Marshal.ReleaseComObject(xlApp);
+                    return new List<string>();
+                }
+
+            }
+
+            //int m = currentFind.Row + 2;
+            //int n = currentFind.Column + 2;
+
+
+            for (int k = 0; k < 10; k++)
+            {
+                var cellValue = ToStr((xlWorksheet.Cells[m + k, n] as Excel.Range).Value2);
+                chemia.Add(cellValue);
+
+            }
+
+
 
 
             //cleanup
@@ -59,6 +118,32 @@ namespace Sandbox
             //quit and release
             xlApp.Quit();
             Marshal.ReleaseComObject(xlApp);
+            //xlApp = null;
+            //xlRange = null;
+            //xlWorkbook = null;
+            //xlWorksheet = null;
+            return chemia;
+        }
+
+        public static string ToStr(object readField)
+        {
+            if ((readField != null))
+            {
+                if (readField.GetType() != typeof(System.DBNull))
+                {
+                    return Convert.ToString(readField);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
         }
     }
+
+
 }
